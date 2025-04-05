dad568ac4ce868a8e2d9bd8a9240799b1395269b
Hemant <guptahemant350@gmail.com>
2025-04-05 15:03:32 +0530

    Added abort controller

--- 4-Social-Media/src/components/PostList.jsx
+++ 4-Social-Media/src/components/PostList.jsx
@@ -9,13 +9,22 @@ const PostList = () => {
 
   useEffect(() => {
     setFetching(true);
-    fetch("https://dummyjson.com/posts")
+
+    const controller = new AbortController();
+    const signal = controller.signal;
+    fetch("https://dummyjson.com/posts" , { signal }) 
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts);
         setFetching(false);
       })
       .catch((err) => console.log(err));
+
+    return () => {
+      console.log("Cleaning up...");
+      controller.abort(); // Abort the fetch request when the component unmounts
+      }
+
       
   }, []);
 
@@ -23,9 +32,9 @@ const PostList = () => {
     <>
       {fetching && <Loading />}
       {!fetching && postList.length === 0 && <WelcomeMessage />}
-      {/* <div className="post-list-container"> */}
+      
       {!fetching && postList.map((post) => <Post key={post.id} post={post} />)}
-      {/* </div> */}
+    
     </>
   );
 };
