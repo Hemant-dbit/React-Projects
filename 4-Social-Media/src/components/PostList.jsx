cd7e167e61a488da2d7720586be817267d8a1734
Hemant <guptahemant350@gmail.com>
2025-04-05 14:54:12 +0530

    Added loading spinner

--- 4-Social-Media/src/components/PostList.jsx
+++ 4-Social-Media/src/components/PostList.jsx
@@ -1,27 +1,31 @@
 import Post from "./Post";
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import Loading from "./Loading";
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
+  const [fetching, setFetching] = useState(false);
 
   useEffect(() => {
+    setFetching(true);
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts);
+        setFetching(false);
       })
       .catch((err) => console.log(err));
+      
   }, []);
 
   return (
     <>
-      {postList.length === 0 && <WelcomeMessage />}
-      <div className="post-list-container">
-        {postList.map((post) => (
-          <Post key={post.id} post={post} />
-        ))}
-      </div>
+      {fetching && <Loading />}
+      {!fetching && postList.length === 0 && <WelcomeMessage />}
+      {/* <div className="post-list-container"> */}
+      {!fetching && postList.map((post) => <Post key={post.id} post={post} />)}
+      {/* </div> */}
     </>
   );
 };
