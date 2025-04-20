931100a8199e3df78e89baa3cf1a7e11d025fb66
Hemant <guptahemant350@gmail.com>
2025-04-20 18:08:35 +0530

    Buttons: Functional

--- 5-Counter-Redux/src/components/Button.jsx
+++ 5-Counter-Redux/src/components/Button.jsx
@@ -1,15 +1,91 @@
+import { useDispatch } from "react-redux";
+import { useRef } from "react";
+
 const Button = () => {
+  const dispatch = useDispatch();
+
+  const addInputRef = useRef();
+  const removeInputRef = useRef();
+
+  const handleIncrement = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+
+  const handleDecrement = () => {
+    dispatch({ type: "DECREMENT" });
+  };
+
+  const handleAdd = () => {
+    dispatch({
+      type: "ADD",
+      payload: Number(addInputRef.current.value),
+    });
+    addInputRef.current.value = ""; // optional: clear input
+  };
+
+  const handleRemove = () => {
+    dispatch({
+      type: "REMOVE",
+      payload: Number(removeInputRef.current.value),
+    });
+    removeInputRef.current.value = ""; // optional: clear input
+  };
+
   return (
-    <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-      <button type="button" className="btn btn-primary">
-        +1
-      </button>
-      
-      <button type="button" className="btn btn-success">
-        -1
-      </button>
-     
-    </div>
+    <>
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mb-3">
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={handleIncrement}
+        >
+          +1
+        </button>
+        <button
+          type="button"
+          className="btn btn-success"
+          onClick={handleDecrement}
+        >
+          -1
+        </button>
+      </div>
+
+      <div className="d-flex justify-content-sm-center gap-3 mt-3 mb-3">
+        <input
+          type="text"
+          placeholder="Enter number"
+          className="form-control"
+          ref={addInputRef}
+          style={{ width: "150px" }}
+        />
+        <button
+          type="button"
+          className="btn btn-primary"
+          style={{ height: "38px", width: "100px" }}
+          onClick={handleAdd}
+        >
+          Add
+        </button>
+      </div>
+
+      <div className="d-flex justify-content-sm-center gap-3 mt-3">
+        <input
+          type="text"
+          placeholder="Enter number"
+          className="form-control"
+          ref={removeInputRef}
+          style={{ width: "150px" }}
+        />
+        <button
+          type="button"
+          className="btn btn-success"
+          style={{ height: "38px", width: "100px" }}
+          onClick={handleRemove}
+        >
+          Remove
+        </button>
+      </div>
+    </>
   );
 };
 
--- 5-Counter-Redux/src/store/index.js
+++ 5-Counter-Redux/src/store/index.js
@@ -1,13 +1,21 @@
 import { createStore } from "redux";
 
 const INITIAL_VALUE = {
-  counter: 5,
+  counter: 0,
 };
 
 
-
-
 const counterReducer = (store = INITIAL_VALUE, action) => {
+
+  if (action.type === "INCREMENT") {
+    return { counter: store.counter + 1 };
+  }else if (action.type === "DECREMENT") {
+    return {counter: store.counter - 1 };
+  }else if (action.type === "ADD") {
+    return { counter: store.counter + action.payload };
+  }else if (action.type === "REMOVE") {
+    return { counter: store.counter - action.payload };
+  }
   return store;
 };
 
